bd350a3a1d62fcb447201d44e8b1d4b5538e2e42
D4rK7355608 <d4rk7355608@gmail.com>
2025-12-13 23:06:59 +0200

    feat: Increment app version
    Bumps the `versionCode` from 80 to 81.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationIdSuffix = ".apptoolkit"
         minSdk = 23
         targetSdk = 36
-        versionCode = 80
+        versionCode = 81
         versionName = "1.1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         @Suppress("UnstableApiUsage")
