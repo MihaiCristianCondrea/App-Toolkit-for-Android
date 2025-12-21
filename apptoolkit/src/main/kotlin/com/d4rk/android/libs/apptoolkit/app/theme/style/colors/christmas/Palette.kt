276abbc56e75a6159ef5ec2fd84efcfab438725a
D4rK7355608 <d4rk7355608@gmail.com>
2025-12-21 08:52:54 +0200

    feat(theme): Add new Christmas color palette
    - Introduces a new `christmas` color theme with a full set of light and dark color definitions, including medium and high-contrast variations.
    - Creates `christmasPalette` by defining `lightColorScheme` and `darkColorScheme` using the new Christmas colors.

---
+++ apptoolkit/src/main/kotlin/com/d4rk/android/libs/apptoolkit/app/theme/style/colors/christmas/Palette.kt
@@ -0,0 +1,152 @@
+package com.d4rk.android.libs.apptoolkit.app.theme.style.colors.christmas
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.ColorPalette
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.backgroundDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.backgroundLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.errorContainerDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.errorContainerLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.errorDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.errorLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.inverseOnSurfaceDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.inverseOnSurfaceLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.inversePrimaryDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.inversePrimaryLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.inverseSurfaceDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.inverseSurfaceLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onBackgroundDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onBackgroundLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onErrorContainerDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onErrorContainerLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onErrorDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onErrorLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onPrimaryContainerDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onPrimaryContainerLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onPrimaryDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onPrimaryLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onSecondaryContainerDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onSecondaryContainerLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onSecondaryDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onSecondaryLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onSurfaceDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onSurfaceLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onSurfaceVariantDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onSurfaceVariantLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onTertiaryContainerDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onTertiaryContainerLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onTertiaryDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.onTertiaryLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.outlineDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.outlineLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.outlineVariantDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.outlineVariantLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.primaryContainerDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.primaryContainerLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.primaryDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.primaryLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.scrimDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.scrimLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.secondaryContainerDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.secondaryContainerLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.secondaryDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.secondaryLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceBrightDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceBrightLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceContainerDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceContainerHighDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceContainerHighLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceContainerHighestDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceContainerHighestLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceContainerLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceContainerLowDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceContainerLowLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceContainerLowestDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceContainerLowestLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceDimDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceDimLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceVariantDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.surfaceVariantLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.tertiaryContainerDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.tertiaryContainerLight
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.tertiaryDark
+import com.d4rk.android.libs.apptoolkit.app.theme.style.colors.red.tertiaryLight
+
+val christmasPalette: ColorPalette = ColorPalette(
+    lightColorScheme = lightColorScheme(
+        primary = primaryLight,
+        onPrimary = onPrimaryLight,
+        primaryContainer = primaryContainerLight,
+        onPrimaryContainer = onPrimaryContainerLight,
+        inversePrimary = inversePrimaryLight,
+        secondary = secondaryLight,
+        onSecondary = onSecondaryLight,
+        secondaryContainer = secondaryContainerLight,
+        onSecondaryContainer = onSecondaryContainerLight,
+        tertiary = tertiaryLight,
+        onTertiary = onTertiaryLight,
+        tertiaryContainer = tertiaryContainerLight,
+        onTertiaryContainer = onTertiaryContainerLight,
+        error = errorLight,
+        onError = onErrorLight,
+        errorContainer = errorContainerLight,
+        onErrorContainer = onErrorContainerLight,
+        background = backgroundLight,
+        onBackground = onBackgroundLight,
+        surface = surfaceLight,
+        onSurface = onSurfaceLight,
+        surfaceVariant = surfaceVariantLight,
+        onSurfaceVariant = onSurfaceVariantLight,
+        outline = outlineLight,
+        outlineVariant = outlineVariantLight,
+        scrim = scrimLight,
+        inverseSurface = inverseSurfaceLight,
+        inverseOnSurface = inverseOnSurfaceLight,
+        surfaceBright = surfaceBrightLight,
+        surfaceDim = surfaceDimLight,
+        surfaceContainerLowest = surfaceContainerLowestLight,
+        surfaceContainerLow = surfaceContainerLowLight,
+        surfaceContainer = surfaceContainerLight,
+        surfaceContainerHigh = surfaceContainerHighLight,
+        surfaceContainerHighest = surfaceContainerHighestLight,
+    ),
+    darkColorScheme = darkColorScheme(
+        primary = primaryDark,
+        onPrimary = onPrimaryDark,
+        primaryContainer = primaryContainerDark,
+        onPrimaryContainer = onPrimaryContainerDark,
+        inversePrimary = inversePrimaryDark,
+        secondary = secondaryDark,
+        onSecondary = onSecondaryDark,
+        secondaryContainer = secondaryContainerDark,
+        onSecondaryContainer = onSecondaryContainerDark,
+        tertiary = tertiaryDark,
+        onTertiary = onTertiaryDark,
+        tertiaryContainer = tertiaryContainerDark,
+        onTertiaryContainer = onTertiaryContainerDark,
+        error = errorDark,
+        onError = onErrorDark,
+        errorContainer = errorContainerDark,
+        onErrorContainer = onErrorContainerDark,
+        background = backgroundDark,
+        onBackground = onBackgroundDark,
+        surface = surfaceDark,
+        onSurface = onSurfaceDark,
+        surfaceVariant = surfaceVariantDark,
+        onSurfaceVariant = onSurfaceVariantDark,
+        outline = outlineDark,
+        outlineVariant = outlineVariantDark,
+        scrim = scrimDark,
+        inverseSurface = inverseSurfaceDark,
+        inverseOnSurface = inverseOnSurfaceDark,
+        surfaceBright = surfaceBrightDark,
+        surfaceDim = surfaceDimDark,
+        surfaceContainerLowest = surfaceContainerLowestDark,
+        surfaceContainerLow = surfaceContainerLowDark,
+        surfaceContainer = surfaceContainerDark,
+        surfaceContainerHigh = surfaceContainerHighDark,
+        surfaceContainerHighest = surfaceContainerHighestDark,
+    ),
+)
