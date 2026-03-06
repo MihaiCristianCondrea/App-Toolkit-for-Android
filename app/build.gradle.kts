6503f1ea63d03d46c62fe78513eafe1fa675c6b5
D4rK7355608 <d4rk7355608@gmail.com>
2026-03-06 11:49:49 +0200

    Update dependencies and optimize imports
    - Updates `androidGradlePlugin` to 9.1.0.
    - Upgrades several AndroidX and Compose dependencies, including `androidxComposeBom` to 2026.02.01, `androidxComposeMaterial3` to 1.5.0-alpha15, and `androidxComposeFoundationLayout` to 1.10.4.
    - Updates `firebaseBom` to 34.10.0, `ktor` to 3.4.1, and `coil` to 3.4.0.
    - Reorganizes and cleans up imports across several UI components and test files, including `FavoriteAppsScreen.kt`, `AppsListScreen.kt`, and `ComponentsScreen.kt`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
         applicationIdSuffix = ".apptoolkit"
         minSdk = 26
         targetSdk = 36
-        versionCode = 106
+        versionCode = 107
         versionName = providers.gradleProperty("PUBLISHING_VERSION").get()
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         @Suppress("UnstableApiUsage")
