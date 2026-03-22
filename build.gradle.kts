e10113dc4efee0a90c39ab429ced2111884b832b
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2026-03-22 19:42:34 +0200

    Scope security constraints to buildscript classpath only

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,3 +26,28 @@ plugins {
     alias(notation = libs.plugins.about.libraries) apply true
     alias(notation = libs.plugins.mannodermaus.android.junit5) apply false
 }
+
+buildscript {
+    dependencies {
+        constraints {
+            classpath("org.jdom:jdom2:2.0.6.1") {
+                because("Mitigates CVE-2021-33813 (XXE) from AGP transitive dependency chain")
+            }
+            classpath("io.netty:netty-handler:4.1.129.Final") {
+                because("Mitigates known Netty handler vulnerabilities (native crash and SNI allocation DoS)")
+            }
+            classpath("io.netty:netty-codec-http2:4.1.129.Final") {
+                because("Mitigates known HTTP/2 vulnerabilities, including Rapid Reset and MadeYouReset")
+            }
+            classpath("io.netty:netty-codec:4.1.129.Final") {
+                because("Mitigates Netty decoder DoS vulnerabilities (including CVE-2025-58057)")
+            }
+            classpath("io.netty:netty-codec-http:4.1.129.Final") {
+                because("Mitigates CRLF injection/request smuggling vulnerability (CVE-2025-67735)")
+            }
+            classpath("org.bitbucket.b_c:jose4j:0.9.6") {
+                because("Mitigates DoS via compressed JWE content (CVE-2024-29371)")
+            }
+        }
+    }
+}
